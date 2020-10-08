3f6f8e665be3671bbae4cd51caa6cfee2bf64e3e
Alex Vihman <alexg657@gmail.com>
2020-10-08 19:34:14 +0300

    link placeholder

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,7 +8,7 @@ const ImageLinkForm = ({onInputChange,onPictureSubmit}) => {
             </p>
             
                 <div className='input_button mw8 center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70' type='text' onChange={onInputChange} />
+                    <input className='f4 pa2 w-70' type='text' onChange={onInputChange} placeholder='Paste link to the image here...'/>
                     <button className='w-30 grow f4 ph3 pv2 dib white bg-dark-blue' onClick={onPictureSubmit}>Detect</button>
                 </div>
             
